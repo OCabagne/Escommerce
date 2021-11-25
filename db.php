08c1f095ee20f70933124887921dfbf3f10bcff5
Oscar-Cabagne <OCabagne@outlook.com>
2021-11-24 20:28:11 -0600

    api v1

--- db.php
+++ db.php
@@ -1,14 +1,36 @@
 <?php
+// Aquí van a estar los querys para todas las consultas, updates, deletes y conexiones
 
 class database
 {
     private $server = "localhost";
     private $user = "user";
     private $pass = "password";
-
-    private function conectar()
+    private $db = "database";
+    
+    public function conectar()
     {  
-        $connect = mysqli_connect($this->server, $this->user, $this->pass);
+        $connect = mysqli_connect($this->server, $this->user, $this->pass, $this->db);
+        
+        return $connect;
+    }
+
+    public function getUserName($userId)
+    {
+        $cnx = new database();
+        $connect = $cnx->conectar();
+
+        $query = "SELECT nombreUsuario FROM usuario WHERE rfc = " + $userId + ";";
+        $exec = mysqli_query($connect, $query);
+    }
+
+    public function eliminarProducto($id_producto)
+    {
+        $cnx = new database();
+        $connect = $cnx->conectar();
+
+        $query = "DELETE * FROM producto WHERE idProducto = " + $id_producto + ";";
+        $exec = mysqli_query($connect, $query);
     }
 }
 
