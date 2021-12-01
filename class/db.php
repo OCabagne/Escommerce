a72d786b5861b0478950a70d6a1714f846953020
Oscar-Cabagne <OCabagne@outlook.com>
2021-11-30 20:01:47 -0600

    Agrego método de desconexión y corrijo las concatenaciones

--- class/db.php
+++ class/db.php
@@ -10,7 +10,18 @@ class database
     
     public function conectar()
     {  
-        return mysqli_connect($this->server, $this->user, $this->pass, $this->dbName);
+        $conexion = mysqli_connect($this->server, $this->user, $this->pass, $this->dbName);
+        if(mysqli_connect_errno())
+        {
+            return false; // Fallo la conexion
+        }
+
+        return $conexion;
+    }
+
+    public function desconectar($conexion)
+    {
+        $conexion->close();
     }
 
     public function getUserName($userId)
@@ -18,17 +29,38 @@ class database
         $cnx = new database();
         $connect = $cnx->conectar();
 
-        $query = "SELECT nombreUsuario FROM usuario WHERE rfc = " + $userId + ";";
+        $query = "SELECT nombreUsuario FROM usuario WHERE rfc = " . $userId . ";";
         $exec = mysqli_query($connect, $query);
+        $cnx->desconectar($connect);
     }
 
-    public function buscarProducto($id_producto)
+    public function confirmarProducto($id_producto)
     {
         $cnx = new database();
         $connect = $cnx->conectar();
-        
-        $query = "SELECT nombreProducto FROM producto WHERE idProducto = " + $id_producto + ";";
-        $exec = mysqli_query($connect, $query);
+        if($connect != false)
+        {           
+            $query = "SELECT nombreProducto FROM producto WHERE idProducto = " . $id_producto . ";";
+            $exec = mysqli_query($connect, $query);
+
+            $cnx->desconectar($connect);
+            return $exec;
+        }
+    }
+
+    public function buscarProducto($campo, $id_producto)
+    {
+        $cnx = new database();  // Conexión con DB
+        $connect = $cnx->conectar();
+        if($connect != false)
+        {
+            $query = "SELECT " . $campo . " FROM producto WHERE idProducto = " . $id_producto . ";";
+            $exec = mysqli_query($connect, $query); // Ejecución del query
+            $row = mysqli_fetch_array($exec); // Obtenemos las columnas resultantes de la consulta
+
+            $cnx->desconectar($connect);   // Desconexión de DB
+            return $row[0]; // Regresamos la primer colúmna de la busqueda (debería ser solo una, pero por precaución se especifica)
+        }
     }
 
     public function eliminarProducto($id_producto)
@@ -38,6 +70,7 @@ class database
 
         $query = "DELETE * FROM producto WHERE idProducto = " + $id_producto + ";";
         $exec = mysqli_query($connect, $query);
+        $cnx->desconectar($connect);
     }
 
     public function login( $email_usuario )
@@ -48,8 +81,8 @@ class database
         $query = "SELECT * FROM usuario WHERE email = " . $email_usuario . ";";
         $exec = mysqli_query( $connect, $query );
 
+        $cnx->desconectar($connect);
         return $exec;
-
     }
 
     public function signup( $id_usuario, $nombre_usuario, $usuario_usuario, $email_usuario, $contra_usuario ) // Tambien puede ser llamada "crearUsuario"
@@ -65,9 +98,25 @@ class database
         }else{
             $msg = "Error al crear";
         }
+
+        $cnx->desconectar($connect);
         return $msg;
     }
 
+    public function categorias()
+    {
+        $cnx = new database();  // Conexión con DB
+        $connect = $cnx->conectar();
+        if($connect != false)
+        {
+            $query = "SELECT nomCategoria FROM categoria;";
+            $exec = mysqli_query($connect, $query); // Ejecución del query
+            $row = mysqli_fetch_array($exec); // Obtenemos las columnas resultantes de la consulta
+
+            $cnx->desconectar($connect);   // Desconexión de DB
+            return $row; // Regresamos el resultado de la consulta
+        }
+    }
 }
 
 ?>
