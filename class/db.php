b9b4625ce63455d3f81f7e97de8b2368293089a1
Oscar-Cabagne <OCabagne@outlook.com>
2021-12-01 18:54:36 -0600

    Agrego método "agregarProducto"

--- class/db.php
+++ class/db.php
@@ -1,5 +1,27 @@
 <?php
+
 // Aquí van a estar los querys para todas las consultas, updates, deletes y conexiones
+/*
+            TODOS LOS MÉTODOS (a excepción de conectar y desconectar) DEBEN SEGUIR ESTA ESTRUCTURA
+
+        $cnx = new database();  // Instancia de db
+        $connect = $cnx->conectar();    // Nos conectamos a la base de datos y guardamos el objeto mysqli retornado en connect.
+        if($connect != false)   // if para cachar el caso de error de conexión.
+        {
+            //
+            // CÓDIGO
+            //
+    
+            $cnx->desconectar($connect);    // Desconectamos de la base de datos.
+            //Si van a seguir haciendo algo, pero ya no necesitan consultas, va a partir de aquí.
+        }
+
+
+        PARA EJECUTAR LAS CONSULTAS SE USA EL MISMO $connect :
+
+            $exec = mysqli_query($connect, $query); // Ejecución del query
+            
+*/
 
 class database
 {
@@ -34,7 +56,7 @@ class database
         $cnx->desconectar($connect);
     }
 
-    public function confirmarProducto($id_producto)
+    public function confirmarProducto($id_producto) // Verifica si un id_producto ya está registrado en la base de datos. 
     {
         $cnx = new database();
         $connect = $cnx->conectar();
@@ -90,23 +112,25 @@ class database
 
     public function signup( $id_usuario, $usuario_usuario, $email_usuario, $contra_usuario, $tipo_usuario ) // Tambien puede ser llamada "crearUsuario"
     {
-        //$cnx = new database();
-        //$connect = $cnx->conectar();
-
-        $query = "INSERT INTO usuario ( rfc, nombreUsuario, contraUsuario, correo, tipoUser) VALUES ( " . $id_usuario . ", " . $usuario_usuario . ", " . $contra_usuario . ", "  . $email_usuario . ", " . $tipo_usuario . ");";
-        $exec = mysqli_query(conectar(), $query);
-        // $exec es true si la consulta fue exitosa, se evalúa en el siguiente if
-        if( $exec ){
-            $msg = "Creado con éxito";
-        }else{
-            $msg = "Error al crear";
+        $cnx = new database();
+        $connect = $cnx->conectar();
+        if($connect != false)   // if para cachar el caso de error de conexión.
+        {
+            $query = "INSERT INTO usuario ( rfc, nombreUsuario, contraUsuario, correo, tipoUser) VALUES ( " . $id_usuario . ", " . $usuario_usuario . ", " . $contra_usuario . ", "  . $email_usuario . ", " . $tipo_usuario . ");";
+            $exec = mysqli_query($connect, $query);
+            // $exec es true si la consulta fue exitosa, se evalúa en el siguiente if
+            if( $exec ){
+                $msg = "Creado con éxito";
+            }else{
+                $msg = "Error al crear";
+            }
+    
+            $cnx->desconectar($connect);
+            return $msg;
         }
-
-        //$cnx->desconectar($connect);
-        return $msg;
     }
 
-    public function categorias()
+    public function categorias()    // Obtener el listado de categorías registradas
     {
         $cnx = new database();  // Conexión con DB
         $connect = $cnx->conectar();
@@ -118,6 +142,21 @@ class database
 
             $cnx->desconectar($connect);   // Desconexión de DB
             return $row; // Regresamos el resultado de la consulta
+        }
+    }
+
+    public function agregarProducto($idCategoria, $precio, $marca, $modelo, $caract)
+    {
+        $cnx = new database();  // Conexión con DB
+        $connect = $cnx->conectar();
+        if($connect != false)
+        {
+            $query = "INSERT INTO producto(idCategoria, precio, marca, modelo, caracteristicas) VALUES (".$idCategoria.",".$precio.",".$marca.",".$modelo.",".$caract.");";
+            $exec = mysqli_query($connect, $query); // Ejecución del query
+            $row = mysqli_fetch_array($exec); // Obtenemos las columnas resultantes de la consulta
+
+            $cnx->desconectar($connect);   // Desconexión de DB
+            return $row; // Regresamos Row. Es false si no se pudo ejecutar
         }
     }
 }
