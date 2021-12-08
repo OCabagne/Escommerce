c8a4e8268725403cf0cb138667317dc1162b1e1f
brandongranados <estrem_@hotmail.com>
2021-12-08 15:55:05 -0600

    PAGINA PRODUCTOS POR ID

--- categorias.sql
+++ categorias.sql
@@ -3,24 +3,24 @@ use escommerce;
 
 
 DELETE FROM `categoria` WHERE 1;
-INSERT INTO `categoria`(`idCategoria`, `nomCategoria`) VALUES (0,'ropaMujer');
-INSERT INTO `categoria`(`idCategoria`, `nomCategoria`) VALUES (1,'ropaHombre');
-INSERT INTO `categoria`(`idCategoria`, `nomCategoria`) VALUES (2,'ropaNino');
-INSERT INTO `categoria`(`idCategoria`, `nomCategoria`) VALUES (3,'ropaNina');
-INSERT INTO `categoria`(`idCategoria`, `nomCategoria`) VALUES (4,'ropaBebe');
+INSERT INTO `categoria`(`idCategoria`, `nomCategoria`) VALUES (1,'ropaMujer');
+INSERT INTO `categoria`(`idCategoria`, `nomCategoria`) VALUES (2,'ropaHombre');
+INSERT INTO `categoria`(`idCategoria`, `nomCategoria`) VALUES (3,'ropaNino');
+INSERT INTO `categoria`(`idCategoria`, `nomCategoria`) VALUES (4,'ropaNina');
+INSERT INTO `categoria`(`idCategoria`, `nomCategoria`) VALUES (5,'ropaBebe');
 
-INSERT INTO `categoria`(`idCategoria`, `nomCategoria`) VALUES (5,'celulares');
-INSERT INTO `categoria`(`idCategoria`, `nomCategoria`) VALUES (6,'audifonos');
-INSERT INTO `categoria`(`idCategoria`, `nomCategoria`) VALUES (7,'monitores');
-INSERT INTO `categoria`(`idCategoria`, `nomCategoria`) VALUES (8,'teclados');
-INSERT INTO `categoria`(`idCategoria`, `nomCategoria`) VALUES (9,'ratones');
+INSERT INTO `categoria`(`idCategoria`, `nomCategoria`) VALUES (6,'celulares');
+INSERT INTO `categoria`(`idCategoria`, `nomCategoria`) VALUES (7,'audifonos');
+INSERT INTO `categoria`(`idCategoria`, `nomCategoria`) VALUES (8,'monitores');
+INSERT INTO `categoria`(`idCategoria`, `nomCategoria`) VALUES (9,'teclados');
+INSERT INTO `categoria`(`idCategoria`, `nomCategoria`) VALUES (10,'ratones');
 
-INSERT INTO `categoria`(`idCategoria`, `nomCategoria`) VALUES (10,'jardin');
-INSERT INTO `categoria`(`idCategoria`, `nomCategoria`) VALUES (11,'muebles');
-INSERT INTO `categoria`(`idCategoria`, `nomCategoria`) VALUES (12,'electrodomesticos');
+INSERT INTO `categoria`(`idCategoria`, `nomCategoria`) VALUES (11,'jardin');
+INSERT INTO `categoria`(`idCategoria`, `nomCategoria`) VALUES (12,'muebles');
+INSERT INTO `categoria`(`idCategoria`, `nomCategoria`) VALUES (13,'electrodomesticos');
 
-INSERT INTO `categoria`(`idCategoria`, `nomCategoria`) VALUES (13,'maquillaje');
-INSERT INTO `categoria`(`idCategoria`, `nomCategoria`) VALUES (14,'skincare');
-INSERT INTO `categoria`(`idCategoria`, `nomCategoria`) VALUES (15,'cremas');
-INSERT INTO `categoria`(`idCategoria`, `nomCategoria`) VALUES (16,'shampoo');
-INSERT INTO `categoria`(`idCategoria`, `nomCategoria`) VALUES (17,'jabones');
+INSERT INTO `categoria`(`idCategoria`, `nomCategoria`) VALUES (14,'maquillaje');
+INSERT INTO `categoria`(`idCategoria`, `nomCategoria`) VALUES (15,'skincare');
+INSERT INTO `categoria`(`idCategoria`, `nomCategoria`) VALUES (16,'cremas');
+INSERT INTO `categoria`(`idCategoria`, `nomCategoria`) VALUES (17,'shampoo');
+INSERT INTO `categoria`(`idCategoria`, `nomCategoria`) VALUES (18,'jabones');
